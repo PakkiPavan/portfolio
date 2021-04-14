b72e4409cd5594c7fd4a9131049a139920b1e5b1
Pakki Pavan <pavanpakki786@gmail.com>
2021-04-14 14:49:23 +0530

    Linkedin project added

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,4 +1,5 @@
 import Youtube from '../../assets/projects_images/Youtube.JPG'
+import Linkedin from '../../assets/projects_images/Linkedin.JPG'
 import Mastermind from '../../assets/projects_images/Mastermind.JPG'
 import Login from '../../assets/projects_images/Login.JPG'
 import Portfolio from '../../assets/projects_images/Portfolio.JPG'
@@ -10,7 +11,7 @@ const data_projects = [
         image: Youtube,
         deployed_url: 'https://utube-clone-using-react.web.app/',
         github_url: 'https://github.com/PakkiPavan/Youtube-clone-react-app',
-        category: ['react.js','redux','firebase']
+        category: ['react.js', 'redux', 'firebase', 'Youtube-API']
     },
     {
         name: 'Mastermind Game',
@@ -18,6 +19,13 @@ const data_projects = [
         deployed_url: 'https://mastermind-game.netlify.app/',
         github_url: 'https://github.com/PakkiPavan/MastermindGame',
         category: ['react.js']
+    },
+    {
+        name: 'Linkedin Clone',
+        image: Linkedin,
+        deployed_url: 'https://linkedin-clone-using-react.web.app/',
+        github_url: 'https://github.com/PakkiPavan/Linkedin-Clone-using-React',
+        category: ['react.js', 'redux', 'firebase']
     },
     {
         name: 'Portfolio',
@@ -31,21 +39,21 @@ const data_projects = [
         image: Login,
         deployed_url: 'https://login-signup-form.netlify.app/',
         github_url: 'https://github.com/PakkiPavan/Login-Signup-Forms',
-        category: ['HTML','CSS','HTML/CSS']
+        category: ['HTML', 'CSS', 'HTML/CSS']
     },
     {
         name: '3D Effect',
         image: Effect,
         deployed_url: 'https://3d-rotation.netlify.app/',
         github_url: 'https://github.com/PakkiPavan/3D-Effect',
-        category: ['HTML','CSS','HTML/CSS']
+        category: ['HTML', 'CSS', 'HTML/CSS']
     },
     {
         name: 'User Authentication',
         image: Login,
         deployed_url: 'https://mern-app-v1.herokuapp.com/',
         github_url: 'https://github.com/PakkiPavan/MERN-App',
-        category: ['node.js', 'mongoDB', 'react.js','heroku']
+        category: ['node.js', 'mongoDB', 'react.js', 'heroku']
     }
 ]
 
